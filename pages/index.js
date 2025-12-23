3a1ed44de7f5831f67e87b79d8a699cbec253230
Paloma Eleuterio <php.eleuterio@gmail.com>
2025-12-23 22:42:42 +0000

    modifica arquivo da  página inicial

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1> Página inicial TabNews 😄 </h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
 export default Home;
